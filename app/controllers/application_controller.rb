8e75b024e0906c831880b9380c7570868d4aff47
Purushottam Deshpande <bornfreeparry@gmail.com>
2021-03-14 18:19:00 +0530

    Token based authentication using devise_auth_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  # protect_from_forgery
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # devise_for :users, defaults: { format: :json }
+
+  # namespace :api do
+  #   scope :v1 do
+  #     mount_devise_token_auth_for 'User', at: 'auth'
+  #   end
+  # end
 end
